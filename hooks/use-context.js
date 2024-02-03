ebf901c4e57a821ebd4c6b6643cf9a7bb64654b8
unknown <abderrahmen.sn93@gmail.com>
2024-02-03 10:45:02 +0100

    Olz2

--- hooks/use-context.js
+++
@@ -1,8 +0,0 @@
-import { useContext } from "react"
-import { AppContext } from "../context/AppContext"
-
-const useGlobalContext = () => {
-  return useContext(AppContext)
-}
-
-export default useGlobalContext;
